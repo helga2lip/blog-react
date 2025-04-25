84932fa14c33d90245f37912b9e7de2f73d04c71
helga2lip <helga2lip@gmail.com>
2025-04-25 17:14:38 +0300

    иконочный шрифт

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import styled from 'styled-components'
 
 export function App() {
+
+  const Div = styled.div`
+color: blue;
+`
 
   return (
     <>
       <div>Hello</div>
+      <Div>Hello</Div>
+      <i className="fa fa-address-book" aria-hidden="true" />
+      <i className="fa fa-free-code-camp" aria-hidden="true" style={{ fontSize: '48px', color: 'orangered' }} />
+
     </>
   )
 }
