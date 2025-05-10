7a1c6a145981dfc0afad0926186337bcdf43a6f6
helga2lip <helga2lip@gmail.com>
2025-05-10 09:59:34 +0300

    основной компонент блога и роуты

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import { Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
 
 export function App() {
 
-  const Div = styled.div`
-color: blue;
-`
+  const Content = styled.div`
+padding: 120px 20px;
+`;
+
+  const H2 = styled.h2`
+text-align: center;
+`;
+
+  const Header = () => {
+    return <div>Header</div>
+  }
+
+  const Footer = () => {
+    return <div>Footer</div>
+  }
 
   return (
     <>
+      <Header />
       <div>Hello</div>
-      <Div>Hello</Div>
-      <i className="fa fa-address-book" aria-hidden="true" />
-      <i className="fa fa-free-code-camp" aria-hidden="true" style={{ fontSize: '48px', color: 'orangered' }} />
-
+      <Content>
+        <H2>Контент страницы</H2>
+        <i className="fa fa-address-book" aria-hidden="true" />
+        <i className="fa fa-free-code-camp" aria-hidden="true" style={{ fontSize: '48px', color: 'orangered' }} />
+        <Routes>
+          <Route path="/" element={<div>Главная страница</div>} />
+          <Route path="/login" element={<div>Авторизация</div>} />
+          <Route path="/register" element={<div>Регистрация</div>} />
+          <Route path="/users" element={<div>Пользователи</div>} />
+          <Route path="/post" element={<div>Новая статья</div>} />
+          <Route path="/post/:postID" element={<div>Статья</div>} />
+          <Route path="*" element={<div>Ошибка</div>} />
+        </Routes>
+      </Content>
+      <Footer />
     </>
   )
 }
